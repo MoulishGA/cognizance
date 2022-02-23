e2fe575d608ad44a5b66dc29bc44518cef0cd68b
MoulishGA <96564641+MoulishGA@users.noreply.github.com>
2022-02-23 16:51:26 +0530

    Add files via upload

---
+++ TASK-4/QUESTION-1.py
@@ -0,0 +1,4 @@
+print("FINDING THE SUM OF TWO NUMBERS;")
+A=int(input("ENTER THE FIRST NUMBER : "))
+B=int(input("ENTER THE SECOND NUMBER : "))
+print("THE SUM OF GIVEN TWO NUMBERS",A,"and",B,"is",A+B)
